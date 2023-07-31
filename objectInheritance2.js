fbc53047462af5d2c49cc8ada5432a4e8fa7f47d
shreyas <shreyasmanolkar123@gmail.com>
2023-07-31 19:09:40 +0530

    prototypal inheritance 2

---
+++ objectInheritance2.js
@@ -0,0 +1,13 @@
+function Person () {
+  this.age = 24;
+}
+
+const me = new Person();
+
+// console.log(me);
+
+// Person.age = 25;
+
+// console.log(Person);
+console.log(Person.age);
+// console.log(me);
